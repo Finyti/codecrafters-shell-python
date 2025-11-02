88d8adc085d597ebbe0c6d9a3045e7199e47d13a
Finyti <122672171+Finyti@users.noreply.github.com>
2025-11-02 15:47:41 +0100

    Implement executing files

--- app/main.py
+++ app/main.py
@@ -1,5 +1,26 @@
 import sys
 import os
+import subprocess
+
+def inCommandDict(command, commandDict):
+    if(type(command) == list): 
+        return str(command[0]) in commandDict
+    return str(command) in commandDict
+
+def InPath(paths, command):
+    for path in paths:
+        if(not os.path.exists(path)):
+            pass
+        # Check if file path exist, and then whether or not it is executable
+        if(os.path.exists(path+f"/{command}")):
+            return path
+    return ''
+
+def isExec(path, command):
+    if(os.access(path+f"/{command}", os.X_OK)):
+        return True
+
+
 
 
 
@@ -17,26 +38,43 @@ def echo(text):
 
 
 
-
-def type(command, commandDict):
+def typeOfArgument(command, commandDict):
     # Check for builtin functions
-    if(command in commandDict):
+    if(inCommandDict(command, commandDict)):
         print(f'{command} is a shell builtin')
         return
     
-    # Check for executables in PATH
     paths=os.environ['PATH'].split(os.pathsep)
-    for path in paths:
-        if(not os.path.exists(path)):
-            pass
-        # Check if file path exist, and then whether or not it is executable
-        if(os.path.exists(path+f"/{command}")):
-            if(os.access(path+f"/{command}", os.X_OK)):
-                print(f'{command} is {path}/{command}')
-                return
+    
+    # Check if file path exist, and then whether or not it is executable
+    path = InPath(paths, command)
+    if(path != ''):
+        if(isExec(path, command)):
+            print(f'{command} is {path}/{command}')
+            return
     print(f'{command}: not found')
 
 
+
+def execute(fullCommand, args):
+    path = ''
+    command = ''
+
+    # if givven command preceeded by full path
+    if(os.path.sep in fullCommand):
+        path = fullCommand.split(os.path.sep)[:-1]
+        path = os.path.sep.join(path)
+        command = fullCommand.split(os.path.sep)[-1]
+    # if givven only the command
+    else:
+        command = fullCommand
+        paths=os.environ['PATH'].split(os.pathsep)
+        path = InPath(paths, command)
+    
+    if(path != '' and isExec(path, command)):
+        os.system(f'{path}{os.path.sep}{command} {" ".join(args)}')
+        
+    
 
 
 def main():
@@ -44,7 +82,8 @@ def main():
         sys.stdout.write("$ ")
         commandDict = {'exit': exitFunc,
                        'echo': echo,
-                       'type': type}
+                       'type': typeOfArgument,
+                       '.': execute}
 
         userInput = input()
         userInputSplit = userInput.split()
@@ -53,8 +92,11 @@ def main():
         # Handles each individual command or exceptions. 
         # If cases are used because each individual command may have need of different atributes 
 
-        if(str(userInputSplit[0]) not in commandDict):
-            sys.stdout.write(f'{userInput}: command not found' + '\n')
+        if(not inCommandDict(userInputSplit, commandDict)):
+            if(userInputSplit[0][0] == '.'):
+                commandDict[userInputSplit[0][0]](userInputSplit[0][1:], userInputSplit[1:])
+            else:
+                sys.stdout.write(f'{userInput}: command not found' + '\n')
         if(userInputSplit[0] == 'exit'):
             commandDict[userInputSplit[0]](userInputSplit[1:])
         if(userInputSplit[0] == 'echo'):
