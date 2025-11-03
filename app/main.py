233f054b25ea8b2edb72e0c905a8c36b7a831d4d
Finyti <122672171+Finyti@users.noreply.github.com>
2025-11-03 10:40:29 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,57 +6,86 @@ def inCommandDict(command, commandDict):
         return str(command[0]) in commandDict
     return str(command) in commandDict
 
-def GetAllFilePaths(paths, command):
-    validPaths = []
-    for path in paths:
-        if(not os.path.exists(path)):
-            pass
-        # Check if file path exist, and then whether or not it is executable
-        if(os.path.exists(path+f"/{command}")):
-            validPaths.append(path)
-    return validPaths
-
-def isExec(path, command):
-    if(os.access(path+f"/{command}", os.X_OK)):
-        return True
-    else:
-        return False
-
-
-def getExec(paths, command):
-    for path in paths:
-        if(path != ''):
-            if(isExec(path, command)):
-                return (command, path)
-    return None
-
 
 def exitFunc(exitStatus):
     if(len(exitStatus) == 0 or int(exitStatus[0]) not in (0, 1)):
         exit(0)
     exit(int(exitStatus[0])) 
 
-
-
-
 def echo(text):
     print(' '.join(text))
     return
 
 
+def GetAllFilePaths(paths, file):
+    """
+    Accepts two variables. \n
+    paths - list of sysytem paths \n
+    file - file to search in sysytem paths \n
 
-def typeOfArgument(command, commandDict={}):
-    # Check for builtin functions
+    Returns a List of all paths where a file was found
+    """
+    validPaths = []
+    for path in paths:
+        if(not os.path.exists(path)):
+            pass
+        # Check if file path exist, and then whether or not it is executable
+        if(os.path.exists(path+f"/{file}")):
+            validPaths.append(path)
+    return validPaths
+
+def isExec(path, command):
+    """
+    Accepts two variables. \n
+    path - a sysytem path \n
+    command - command to search in sysytem path \n
+
+    Returns a True if command exist and is executable by provided path \n
+    or False otherwise
+    """
+    if(os.access(path+f"/{command}", os.X_OK)):
+        return True
+    else:
+        return False
+
+
+def getExec(paths, exec):
+    """
+    Accepts two variables. \n
+    'paths' - list of sysytem paths \n
+    'exec' - command to search in sysytem paths \n \n
+
+    If exec exists returns a touple with [0] being 'exec' name and [1] being valid path \n
+    or None otherwise
+    """
+    for path in paths:
+        if(path != ''):
+            if(isExec(path, exec)):
+                return (exec, path)
+    return None
+
+
+
+
+def typeOfArgument(command, commandDict):
+    """
+    Accepts two variables. \n
+    'command' - command to check \n
+    'commandDict' - Dictionary with all builtin commands \n \n
+
+    If command is an executable, returns a touple with [0] being 'command' name and [1] being valid path \n
+    or None in any other case
+    """
+    # Check if command is a builtin functions
     if(inCommandDict(command, commandDict)):
         print(f'{command} is a shell builtin')
         return
     
+    # If not builtin, check if command is a an executable accesible in any of the locations in PATH
     paths=os.environ['PATH'].split(os.pathsep)
-    
-    # Check if file path exist, and then whether or not it is executable
-    # validPaths = GetAllFilePaths(paths, command)
     validExec = getExec(paths, command)
     if(validExec != None):
+        print(f'{command} is {validExec[1]}/{command}')
         return (command, validExec[1])
 
     print(f'{command}: not found')
@@ -65,25 +94,32 @@ def typeOfArgument(command, commandDict={}):
 
 
 def execute(fullCommand, args):
+    """
+    Accepts two variables. \n
+    'fullCommand' - string of user input, may contain full execx path, or just the name \n
+    'args' - List of arguments to be passed when executing the command \n \n
+
+    If command is an executable, executes it with given arguments
+    Returns True if execution happened, False otherwise
+    """
+    path = ''
+    command = ''
 
     if(fullCommand[0] == '.'):
         fullCommand = fullCommand[1:]
-    path = ''
-    command = ''
 
     # if givven command preceeded by full path
     if(os.path.sep in fullCommand):
         path = fullCommand.split(os.path.sep)[:-1]
         path = os.path.sep.join(path)
         command = fullCommand.split(os.path.sep)[-1]
-    # if givven only the command
+    # if givven only the command name
     else:
         command = fullCommand
-        # paths=os.environ['PATH'].split(os.pathsep)
         paths=os.environ['PATH'].split(os.pathsep)
-        execValidity = getExec(paths, command)
-        if(execValidity != None):
-            path = execValidity[1]
+        validExec = getExec(paths, command)
+        if(validExec != None):
+            path = validExec[1]
     if(path != '' and isExec(path, command)):
         os.system(f'{command} {" ".join(args)}')
         return True
@@ -116,10 +152,7 @@ def main():
         if(userInputSplit[0] == 'echo'):
             commandDict[userInputSplit[0]](userInputSplit[1:])
         if(userInputSplit[0] == 'type'):
-            typeReturn = commandDict[userInputSplit[0]](userInputSplit[1], commandDict)
-            if(typeReturn != None):
-                print(f'{typeReturn[0]} is {typeReturn[1]}/{typeReturn[0]}')
-
+            commandDict[userInputSplit[0]](userInputSplit[1], commandDict)
 
 
 
